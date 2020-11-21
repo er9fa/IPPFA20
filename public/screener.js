fdc7dc35deb921b6ac6b31e5f1a0c5e4432ef681
hc5bu <hc5bu@virginia.edu>
2020-11-21 17:00:15 -0500

    Added Screener.js

---
+++ public/screener.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const parseScreenerResponse = (html) => {
+  const $ = cheerio.load(html);
+  const rows = $(".table-dark-row-cp, .table-light-row-cp")//$('#screener-content > table > tbody > tr > table > tbody > tr, .screener-body-table-nw');
+  const data = new Set();
+  console.log(rows);
+  rows.each((i, row) => {
+    const cell = $(row).find('> td:nth-child(2)');
+    if (cell.length) {
+      console.log(cell);
+      data.add(cell.text());
+    }
+  });
+  return [...data];
+};
+
+const screener = async (url) => {
+  const { data } = await axios.get(url);
+  const results = parseScreenerResponse(data);
+  console.log(results);
+  return results;
+};
+
+//module.exports = screener;
+//screener("https://finviz.com/screener.ashx?v=111&f=fa_pe_high,sh_price_20to50");
